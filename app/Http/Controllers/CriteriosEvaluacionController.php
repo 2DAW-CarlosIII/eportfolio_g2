894aaa8a6e12f83f9a3470108422fb6351c1686e
FATIMA ZAHRA <1728829@alu.murciaeduca.es>
2025-12-09 08:45:45 +0100

    actualizacionTablaCriterios

--- app/Http/Controllers/CriteriosEvaluacionController.php
+++ app/Http/Controllers/CriteriosEvaluacionController.php
@@ -7,24 +7,39 @@ use Illuminate\Http\Request;
 
 class CriteriosEvaluacionController extends Controller
 {
-    public function getIndex(){
+    public function getIndex()
+    {
         return view('criterios-evaluacion.index')
             ->with('criterios_evaluacion', CriterioEvaluacion::all());
     }
 
-    public function getShow($id){
+    public function getShow($id)
+    {
         return view('criterios-evaluacion.show')
             ->with('criterios_evaluacion', CriterioEvaluacion::findOrFail($id))
             ->with('id', $id);
     }
 
-    public function getCreate(){
+    public function getCreate()
+    {
         return view('criterios-evaluacion.create');
     }
 
-    public function getEdit($id){
+    public function getEdit($id)
+    {
         return view('criterios-evaluacion.edit')
             ->with('criterios_evaluacion', CriterioEvaluacion::findOrFail($id))
             ->with('id', $id);
+    }
+    public function postCreate(Request $request)
+    {
+        $criterioEvaluacion = CriterioEvaluacion::create($request->all());
+        return redirect()->action([self::class, 'getShow'], ['id' => $criterioEvaluacion->id]);
+    }
+    public function postEdit(Request $request, $id)
+    {
+        $criterioEvaluacion = CriterioEvaluacion::findOrFail($id);
+        $criterioEvaluacion->update($request->all());
+        return redirect()->action([self::class, 'getShow'], ['id' => $criterioEvaluacion->id]);
     }
 }
